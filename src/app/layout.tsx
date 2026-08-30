97dd34974bc8e1053dbe5571aa667d0057e630cd
leandrodiasmoreira4-afk <leandro.dias.moreira4@gmail.com>
2026-08-30 19:09:10 -0300

    fix: load visual enhancements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Cormorant_Garamond, Inter } from "next/font/google";
 import "./globals.css";
+import "./enhancements.css";
 import { Header } from "@/components/layout/Header";
 import { Footer } from "@/components/layout/Footer";
 import { WhatsAppFloating } from "@/components/layout/WhatsAppFloating";
